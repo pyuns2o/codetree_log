8d6cf53160067ab4d89463b5b86045947d18d013
pyuns2o <96762688+pyuns2o@users.noreply.github.com>
2025-01-24 19:44:40 +0900

    Title: 최대공약수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250124/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+# Write your code here!
+def gcd(n, m):
+    for i in range(min(n, m), 0, -1):
+        if n % i == 0 and m % i == 0:
+            print(i)
+            break
+
+gcd(n, m)
