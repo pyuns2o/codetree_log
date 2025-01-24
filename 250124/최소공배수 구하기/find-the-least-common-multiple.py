4a17338cfb757e4e047784787fe69478a787dc65
pyuns2o <96762688+pyuns2o@users.noreply.github.com>
2025-01-24 20:46:15 +0900

    Title: 최소공배수 구하기, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250124/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250124/최소공배수 구하기/find-the-least-common-multiple.py
@@ -3,6 +3,7 @@ n, m = map(int, input().split())
 # Write your code here!
 
 def lcm(n,m):
+    gcd = 0
     for i in range(min(n,m), 0, -1):
         if n % i == 0 and m % i == 0:
             gcd = i
@@ -10,12 +11,3 @@ def lcm(n,m):
     return (n * m) // gcd
 
 print(lcm(n, m))
-
-# def gcd(n,m):
-#     for i in range(min(n,m), 0, -1):
-#         if n % i == 0 and m % i == 0:
-#             return i
-# def lcm(n,m):
-#     return n * m // gcd(n,m)
-    
-# print(lcm(n,m))
