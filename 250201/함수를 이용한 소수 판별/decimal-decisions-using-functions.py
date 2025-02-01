30e5a8766d288442e9ffeab9bd74bcca15468b53
pyuns2o <96762688+pyuns2o@users.noreply.github.com>
2025-02-01 16:55:11 +0900

    Title: 함수를 이용한 소수 판별, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250201/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+
+# Write your code here!
+
+# 소수 판별 함수 True/ False
+def sosu(n):
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+
+# 소수 합산 함수 True 이면 합산
+def sosu_sum(a,b):
+    summ = 0
+    for i in range(a, b+1):
+        if sosu(i):
+            summ += i
+    
+    return summ
+
+print(sosu_sum(a,b))
