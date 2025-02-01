4c3875706faadd5a8baa270c76e912450ef97c40
pyuns2o <96762688+pyuns2o@users.noreply.github.com>
2025-02-01 16:24:06 +0900

    Title: 함수를 이용한 369 게임, Time: 100ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250201/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,18 @@
+a, b = map(int, input().split())
+
+# Write your code here!
+
+def test(a,b):
+    cnt = 0
+    for num in range(a,b+1):
+        if num % 3 == 0:
+            cnt += 1
+        else:
+            s = str(num)
+            for i in s:
+                if (i=='3') or (i=='6') or (i=='9'):
+                    cnt += 1
+    return cnt
+
+print(test(a,b))
+
